f9504506ba2ffa8fcf886707c1c84f15a9d6521e
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2023-01-29 12:45:13 +0100

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import math
+""" this module takes in a number and returns the pascal triangle for that number """
 
 def nCr(num, re):
+    """ method to create the combination """
     factN = math.factorial(num);
     factRe = math.factorial(re);
     factN_re = math.factorial(num - re);
@@ -10,6 +12,7 @@ def nCr(num, re):
     return result;
 
 def pascal_triangle(num):
+    """ method to create the pascal triangle """
     triangle = [];
     if (num == 0):
         list = []
